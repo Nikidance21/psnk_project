6fddf50f5619e6ed0a32255ddfbf66863c3c5a64
Никита Авдеенко <gr711_avndm@lab-407-10.tomtit-tomsk.ru>
2023-03-06 10:59:12 +0700

    1-4

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        description: 'A pair of warm, fuzzy socks',
+        image: "./assets/vmSocks-blue-onWhite.jpg",
+        altText: "A pair of socks",
+        DopText: "More products like this.",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        inStock: true,
+        invertory: 100,
+        onSale: true
+
+    }
+
+})
